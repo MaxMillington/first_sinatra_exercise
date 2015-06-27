6a7b61ac52bb8a3a12d2de9ca552975e0ae06fa2
MaxMillington <maxmillington89@gmail.com>
2015-06-26 19:16:46 -0600

    colors and web guessing

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,38 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number ||= rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  color = pick_color(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 end
 
+def check_guess(guess)
+  difference = (SECRET_NUMBER - guess.to_i).abs
+  if guess == nil
+    ""
+  elsif guess.to_i > SECRET_NUMBER
+    difference > 5 ? "Way too high!": "Too high!"
+  elsif guess.to_i < SECRET_NUMBER
+    difference > 5 ? "Way too low!": "Too low!"
+  else
+    "You win!\n
+      The SECRET NUMBER is #{SECRET_NUMBER}!"
+  end
+end
+
+def pick_color(guess)
+  difference = (SECRET_NUMBER - guess.to_i).abs
+  if guess == nil
+    ""
+  elsif guess.to_i > SECRET_NUMBER
+    difference > 5 ? "red": "orange"
+  elsif guess.to_i < SECRET_NUMBER
+    difference > 5 ? "red": "pink"
+  elsif guess.to_i == SECRET_NUMBER
+    "green"
+  end
+end
